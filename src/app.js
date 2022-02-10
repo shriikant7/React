f3c33e3ffc240dd344d2b040c62ffd491512fc8e
Your Name <you@example.com>
2022-02-10 22:34:24 +0530

    First commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Container from "./record-collection/container";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Container />
+    </React.Fragment>
+  );
+};
+
+export default App;
